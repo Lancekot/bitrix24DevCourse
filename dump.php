a95f24e5e4d3e0a1d278227f80169cf4e8cb2b89
Konstantin <konstantin.markin88@yandex.ru>
2024-11-24 23:32:50 +0300

    homework 3

--- dump.php
+++ dump.php
@@ -1,23 +1,39 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 
+use Bitrix\Main\Diag\Debug;
+use Bitrix\Main\Loader;
+use Bitrix\Main\Application;
+
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 /**
  * @var CMain $APPLICATION
  */
-$APPLICATION->setTitle('Отладка пример');
+$APPLICATION->setTitle('Отладка Sql-запроса');
 
-\Bitrix\Main\Diag\Debug::dump([
-    1,
-    2,
-    3,
-    4,
-    6,
-    -4,
-    -2,
-    -11,
-    111,
-    -11
+
+Loader::includeModule('iblock');
+
+Application::getConnection()->startTracker();
+
+$query = \Bitrix\Iblock\ElementTable::getList([
+    'filter' => [
+        'IBLOCK_ID' => 14,
+    ],
+    'select' => [
+        'ID'
+    ]
 ]);
+
+Application::getConnection()->stopTracker();
+
+Debug::dump($query->getTrackerQuery()->getSql());
+
+
+
+
+
+
 
 
 
--- local/php_interface/classes/Otus/Diagnostic/OtusFileExceptionHandlerLog.php
+++ local/php_interface/classes/Otus/Diagnostic/OtusFileExceptionHandlerLog.php
@@ -9,6 +9,21 @@ class OtusFileExceptionHandlerLog extends FileExceptionHandlerLog
 {
     public function write($exception, $logType)
     {
+        $text = ExceptionHandlerFormatter::format($exception);
 
+        $context = [
+            'type' => static::logTypeToString($logType),
+        ];
+
+        $logLevel = static::logTypeToLevel($logType);
+        $message = "{date} - Host: {host} - {type} - {$text}\n";
+        $lines = explode("\n", $message);
+
+        foreach($lines as &$line) {
+            $line = 'OTUS - ' . $line;
+        }
+        $message = implode("\n", $lines);
+
+        $this->logger->log($logLevel, $message, $context);
     }
 }
