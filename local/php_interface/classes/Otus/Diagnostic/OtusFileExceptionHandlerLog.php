667c8d5df223b3503774c63a3beef623f7fc3da4
Konstantin <kmlancekot@gmail.com>
2024-11-24 22:48:33 +0500

    Upload init

---
+++ local/php_interface/classes/Otus/Diagnostic/OtusFileExceptionHandlerLog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Otus\Diagnostic;
+
+use Bitrix\Main\Diag\FileExeptionHandlerLog;
+use Bitrix\Main\Diag\ExceptionHandlerFormatter;
+
+class OtusFileExceptionHandlerLog extends FileExceptionHandlerLog
+{
+    public function write($exception, $logType)
+    {
+
+    }
+}
---
+++ local/php_interface/init.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DEBUG_FILE_NAME', $_SERVER['DOCUMENT_ROOT'].'/logs/' . date("Y-m-d") . '.log');
+
+if(file_exists(__DIR__."/config.php". '/classes/autoload.php')){
+    require_once __DIR__. '/classes/autoload.php';
+}
+
+
+
+
+
+
